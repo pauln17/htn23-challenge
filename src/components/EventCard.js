91e8733eb94c08aa34446e06461bb98afbb81c2e
Paul <paulnguyen0417@gmail.com>
2023-02-20 12:34:20 -0500

    Responsive Fixes w/ Card Height

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -18,21 +18,25 @@ const EventCard = ({ event: { description, event_type, id, name, permission, pri
 
     return (
         <>
-            <Card className="card" style={{ width: '18rem' }}>
+            <Card className="card h-100 w-100" style={{ width: '18rem' }}>
                 <a target="_blank" className="card-info-link" {...public_url && {href: public_url}}>
-                    <Card.Img className="card-img" variant="top" src='https://i.imgur.com/SZFa3wf.png'/>
-                    <Card.Body>
-                        <Card.Title className="card-title">{name}</Card.Title>
-                        <Card.Text className="event-type">{toProperCase(event_type)}</Card.Text>
-                        <Card.Text className="card-text">{description}</Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroup.Item></ListGroup.Item>
-                        {speakers[0] && (
-                            <ListGroup.Item><strong>Speaker:</strong> {speakers[0].name}</ListGroup.Item>
-                        )}                
-                            <ListGroup.Item><strong>Start Time (EST):</strong> {new Date(start_time).toLocaleString('en-US', { year: 'numeric', month: 'numeric', day : 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'})}</ListGroup.Item>
-                    </ListGroup>
+                    <div className="cards-content-container">
+                        <div>
+                            <Card.Img className="card-img" variant="top" src='https://i.imgur.com/SZFa3wf.png'/>
+                            <Card.Body>
+                                <Card.Title className="card-title">{name}</Card.Title>
+                                <Card.Text className="event-type">{toProperCase(event_type)}</Card.Text>
+                                <Card.Text className="card-text">{description}</Card.Text>
+                            </Card.Body>
+                        </div>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item></ListGroup.Item>
+                            {speakers[0] && (
+                                <ListGroup.Item><strong>Speaker:</strong> {speakers[0].name}</ListGroup.Item>
+                            )}                
+                                <ListGroup.Item><strong>Start Time (EST):</strong> {new Date(start_time).toLocaleString('en-US', { year: 'numeric', month: 'numeric', day : 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'})}</ListGroup.Item>
+                        </ListGroup>
+                    </div>
                 </a>
             </Card> 
         </>
