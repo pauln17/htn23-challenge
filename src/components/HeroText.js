0e34b6828e75f370c2342f77d526a33c0a086b05
Paul <paulnguyen0417@gmail.com>
2023-02-20 14:51:36 -0500

    Colored Words in Hero & Background Color Change

--- src/components/HeroText.js
+++ src/components/HeroText.js
@@ -9,7 +9,7 @@ const HeroText = () => {
     return (
         <div className="section container">
             <div className="container-fluid d-flex flex-column justify-content-center align-items-center text-center mt-5">
-                <h1 className="hero-text">Hack the North is Canada's largest hackathon where anyone can spark their passion for {' '}
+                <h1 className="hero-text">Hack the North is Canada's <span className="word-1">largest</span> hackathon where <span className="word-2">anyone</span> can <span className="word-3">spark</span> their passion for {' '}
                     <Typed
                     className="type-animation-strings"
                     strings = {[
